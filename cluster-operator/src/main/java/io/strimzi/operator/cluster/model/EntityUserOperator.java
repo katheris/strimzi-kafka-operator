cbe5e1ee98621d4ead5dfe79034359032743dd55
Kate Stanley <11195226+katheris@users.noreply.github.com>
2026-07-30 16:57:18 +0100

    Update EntityUserOperator to pass env vars for cert-manager
    Signed-off-by: Kate Stanley <11195226+katheris@users.noreply.github.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/EntityUserOperator.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/EntityUserOperator.java
@@ -17,7 +17,9 @@ import io.fabric8.kubernetes.api.model.rbac.RoleRefBuilder;
 import io.fabric8.kubernetes.api.model.rbac.Subject;
 import io.fabric8.kubernetes.api.model.rbac.SubjectBuilder;
 import io.strimzi.api.kafka.model.common.CertificateAuthority;
+import io.strimzi.api.kafka.model.common.CertificateManagerType;
 import io.strimzi.api.kafka.model.common.JvmOptions;
+import io.strimzi.api.kafka.model.common.certmanager.IssuerKind;
 import io.strimzi.api.kafka.model.common.template.PodTemplate;
 import io.strimzi.api.kafka.model.common.template.ResourceTemplate;
 import io.strimzi.api.kafka.model.kafka.Kafka;
@@ -68,6 +70,10 @@ public class EntityUserOperator extends AbstractModel implements SupportsLogging
     /* test */ static final String ENV_VAR_SECRET_PREFIX = "STRIMZI_SECRET_PREFIX";
     /* test */ static final String ENV_VAR_ACLS_ADMIN_API_SUPPORTED = "STRIMZI_ACLS_ADMIN_API_SUPPORTED";
     /* test */ static final String ENV_VAR_MAINTENANCE_TIME_WINDOWS = "STRIMZI_MAINTENANCE_TIME_WINDOWS";
+    /* test */ static final String ENV_VAR_CA_TYPE = "STRIMZI_CA_TYPE";
+    /* test */ static final String ENV_VAR_CM_ISSUER_NAME = "STRIMZI_CM_ISSUER_NAME";
+    /* test */ static final String ENV_VAR_CM_ISSUER_KIND = "STRIMZI_CM_ISSUER_KIND";
+    /* test */ static final String ENV_VAR_CM_ISSUER_GROUP = "STRIMZI_CM_ISSUER_GROUP";
 
     // Volume name of the temporary volume used by the UO container
     // Because the container shares the pod with other containers, it needs to have a unique name
@@ -87,6 +93,10 @@ public class EntityUserOperator extends AbstractModel implements SupportsLogging
     private List<String> maintenanceWindows;
     private LoggingModel logging;
     private boolean generatePkcs12Stores;
+    private CertificateManagerType certificateManagerType;
+    private String certManagerIssuerName;
+    private IssuerKind certManagerIssuerKind;
+    private String certManagerIssuerGroup;
 
     /**
      * Constructs a new EntityUserOperator.
@@ -145,6 +155,7 @@ public class EntityUserOperator extends AbstractModel implements SupportsLogging
             result.livenessProbeOptions = ProbeUtils.extractLivenessProbeOptionsOrDefault(userOperatorSpec, EntityOperator.DEFAULT_HEALTHCHECK_OPTIONS);
             result.featureGatesEnvVarValue = config.featureGates().toEnvironmentVariable();
             result.generatePkcs12Stores = config.isPkcs12KeystoreGeneration();
+            result.certificateManagerType = CertificateManagerType.STRIMZI_IO;
 
             if (kafkaAssembly.getSpec().getEntityOperator().getTemplate() != null)  {
                 result.templateRoleBinding = kafkaAssembly.getSpec().getEntityOperator().getTemplate().getUserOperatorRoleBinding();
@@ -157,6 +168,14 @@ public class EntityUserOperator extends AbstractModel implements SupportsLogging
 
                 if (kafkaAssembly.getSpec().getClientsCa().getRenewalDays() > 0) {
                     result.clientsCaRenewalDays = kafkaAssembly.getSpec().getClientsCa().getRenewalDays();
+                }
+                result.certificateManagerType = kafkaAssembly.getSpec().getClientsCa().getType();
+                if (CertificateManagerType.CERT_MANAGER_IO.equals(result.certificateManagerType)
+                        && kafkaAssembly.getSpec().getClientsCa().getCertManager() != null
+                        && kafkaAssembly.getSpec().getClientsCa().getCertManager().getIssuerRef() != null) {
+                    result.certManagerIssuerName = kafkaAssembly.getSpec().getClientsCa().getCertManager().getIssuerRef().getName();
+                    result.certManagerIssuerKind = kafkaAssembly.getSpec().getClientsCa().getCertManager().getIssuerRef().getKind();
+                    result.certManagerIssuerGroup = kafkaAssembly.getSpec().getClientsCa().getCertManager().getIssuerRef().getGroup();
                 }
             }
 
@@ -211,6 +230,7 @@ public class EntityUserOperator extends AbstractModel implements SupportsLogging
         varList.add(ContainerUtils.createEnvVar(ENV_VAR_SECRET_PREFIX, secretPrefix));
         varList.add(ContainerUtils.createEnvVar(ENV_VAR_ACLS_ADMIN_API_SUPPORTED, String.valueOf(aclsAdminApiSupported)));
         varList.add(ContainerUtils.createEnvVar(ClusterOperatorConfig.PKCS12_KEYSTORE_GENERATION.key(), String.valueOf(generatePkcs12Stores)));
+        varList.add(ContainerUtils.createEnvVar(ENV_VAR_CA_TYPE, certificateManagerType.toValue()));
         JvmOptionUtils.javaOptions(varList, jvmOptions);
 
         // Add feature gates configuration if not empty
@@ -227,6 +247,19 @@ public class EntityUserOperator extends AbstractModel implements SupportsLogging
         if (maintenanceWindows != null && !maintenanceWindows.isEmpty())    {
             // The Cron expressions can contain commas -> we use semi-colon as delimiter
             varList.add(ContainerUtils.createEnvVar(ENV_VAR_MAINTENANCE_TIME_WINDOWS, String.join(";", maintenanceWindows)));
+        }
+
+        // if CA type is cert-manager, set cert-manager env vars
+        if (CertificateManagerType.CERT_MANAGER_IO.equals(certificateManagerType)) {
+            if (certManagerIssuerName != null && !certManagerIssuerName.isEmpty()) {
+                varList.add(ContainerUtils.createEnvVar(ENV_VAR_CM_ISSUER_NAME, certManagerIssuerName));
+            }
+            if (certManagerIssuerKind != null) {
+                varList.add(ContainerUtils.createEnvVar(ENV_VAR_CM_ISSUER_KIND, certManagerIssuerKind.toValue()));
+            }
+            if (certManagerIssuerGroup != null && !certManagerIssuerGroup.isEmpty()) {
+                varList.add(ContainerUtils.createEnvVar(ENV_VAR_CM_ISSUER_GROUP, certManagerIssuerGroup));
+            }
         }
 
         return varList;
